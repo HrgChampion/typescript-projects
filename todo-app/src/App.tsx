424e0841c6f3977ef4f888841ecd2e6ed4625a9e
Himanshu <hgauba4@gmail.com>
2022-05-07 21:50:16 +0530

    TodoList added

--- todo-app/src/App.tsx
+++ todo-app/src/App.tsx
@@ -1,18 +1,34 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import InputField from './components/InputField/InputField'
+import { Todo } from './components/model'
 
 
 
 
 const App:React.FC=()=>{ //React.FC is a function component
   const [todo,setTodo]=useState<string>('')
+const [todos,setTodos]=useState<Todo[]>([])
 
+const handleAdd=(e:React.FormEvent)=>{
+  e.preventDefault()
+  if(todo){
+    setTodos([...todos,{
+      id:Date.now(),
+      todo,
+      isDone:false
+    }])
+    setTodo("")
+  }
+}
   return (
     <div className="App">
      <span className='heading'>Todo-App</span>
-     <InputField  todo={todo} setTodo={setTodo}/>
+     <InputField  todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
+     {todos.map((t)=>(
+       <li>{t.todo}</li>
+     ))}
     </div>
   )
 }
--- todo-app/src/components/InputField/InputField.tsx
+++ todo-app/src/components/InputField/InputField.tsx
@@ -1,18 +1,24 @@
 
-import React from 'react'
+import React, { useRef } from 'react'
 import "./InputField.css"
 interface Props{
-    todo:string,
-    setTodo:React.Dispatch<React.SetStateAction<string>>
+    todo:string;
+    setTodo:React.Dispatch<React.SetStateAction<string>>;
+    handleAdd:(e:React.FormEvent)=>void;
 
 }
 
-const InputField:React.FC<Props> = ({todo,setTodo}:Props) => {
+const InputField:React.FC<Props> = ({todo,setTodo,handleAdd}:Props) => {
+    const inputRef=useRef<HTMLInputElement>(null);
   return (
     <div>
-        <form className='input'>
+        <form className='input' onSubmit={(e)=>{handleAdd(e)
+    inputRef.current?.blur()    
+    }}
+        
+        >
 
-            <input type="input" value={todo} placeholder='Enter Task Todo' className='input_box' 
+            <input type="input"ref={inputRef} value={todo} placeholder='Enter Task Todo' className='input_box' 
             onChange={(e)=>setTodo(e.target.value)}/>
            
             <button className='input_submit' type='submit'>Add</button>
