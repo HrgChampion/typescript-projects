0d50625dda1b0202dc160ae11d545c979b5a987b
Himanshu Gauba <himanshu.gauba@decisionpoint.in>
2022-06-01 11:42:43 +0530

    learning-ts

--- project-1/src/index.ts
+++ project-1/src/index.ts
@@ -3,8 +3,31 @@
 // greet=()=>{
 //     console.log("Hello");
 // }
-const add=(a:number,b:number,c:number|string=10)=>{
-    console.log(c);
-    return a+b;
-}
-console.log(add(1, 2,20));
+// const add=(a:number,b:number,c:number|string=10)=>{
+//     console.log(c);
+//     return a+b;
+// }
+// console.log(add(1, 2,20));
+
+// const minus=(a:number,b:number)=>{
+//     return a-b;
+// }
+// let result=minus(10,5);
+
+// type stringorNum=string|number;
+// type objWithName={name:string,uid:stringorNum};
+
+// const logDEtails=(uid:stringorNum,item:string)=>{
+//     console.log(`${item} with ${uid}`);
+// }
+
+
+// const greet=(user:objWithName)=>{
+//     console.log(user.name);
+//     console.log(user.uid);
+// }
+// const greetAgain=(user:objWithName)=>{
+//     console.log(user.name);
+//     console.log(user.uid);
+// }
+
